60feb8edaed5b29731ebb5fb18dde2f6965cab65
sutr-app <91235033+sutr-app@users.noreply.github.com>
2024-02-19 19:36:06 +0900

    remove unuse unstable feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,2 @@
-// use pattern feature (may be unstable)
-// https://github.com/rust-lang/rust/issues/27721
-// (10 years passed, still unstable. So implement by myself for necessary case only)
-#![cfg_attr(feature = "unstable", feature(pattern))]
-
 pub mod text;
 pub mod util;
